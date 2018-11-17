af1db64940542109811550d7e499f75ef20b6099
Nicholas Wallace <nicholaslwallace@gmail.com>
2018-11-16 23:55:19 -0600

    class skills updated

--- src/classskill..cpp
+++ src/classskill..cpp
@@ -1,130 +1,43 @@
 Barbarianskills {          //choose two
-	animalhandling,
-	athleticss,
-	intimidation,
-	nature,
-	perception,
-	survival
+	"animalhandling","athleticss","intimidation","nature","perception","survival"
 }
 
 Bardskills {             //choose three
-    acrobatics,
-	animalhandling,
-	arcana,
-	athletics,
-	deception,
-	history,
-	insight,
-	intimidation,
-	investigation,
-	medicine,
-	nature,
-	perception,
-	performance,
-	persuasion,
-	religion,
-	sleightofhand,
-	stealth,
-	survival
+    "acrobatics","animalhandling","arcana","athletics","deception","history","insight","intimidation","investigation","medicine","nature","perception","performance","persuasion","religion","sleightofhand","stealth","survival"
 }
 
 Claricskills {                //choose two
-	history,
-	insight,
-	medicine,
-	persuasion,
-	religion
+	"history","insight","medicine","persuasion","religion"
 }
 
 Druidskills {                   //choose two
-	arcana,
-	animalhandling,
-	insight,
-	medicine,
-	nature,
-	perception,
-	religion,
-	survival
-}
-
-Fighterskills {                     //choose two
-	acrobatics,
-	animalhandling,
-	athleticss,
-	history,
-	insight,
-	intimidation,
-	perception,
-	survival
+	"arcana","animalhandling","insight","medicine","nature","perception","religion","survival"
 }
  
 Monkskills {                           //Choose two
-	acrobatics,
-	athletics,
-	history,
-	insight,
-	religion,
-	stealth
+	"acrobatics","athletics","history","insight","religion","stealth"
 }
 
 Paladin {                              //choose two
-	athletics,
-	insight,
-	intimidation,
-	medicine,
-	persuasion,
-	religion
+	"athletics","insight","intimidation","medicine","persuasion","religion"
 }
 
 Rangerskills {                           //choose three
-	animalhandling,
-	athletics,
-	insight,
-	investigation,
-	nature,
-	perception,
-	stealth,
-	survival
+	"animalhandling","athletics","insight","investigation","nature","perception","stealth","survival"
 }
 
 Rogueskills {                             //choose four
-	acrobatics,
-	athletics,
-	deception,
-	insight,
-	intimidation,
-	investigation,
-	perception,
-	performance,
-	persuasion,
-	sleightofhand,
-	stealth
+	"acrobatics","athletics","deception","insight","intimidation","investigation","perception","performance","persuasion","sleightofhand","stealth"
 }
 
 Sorcererskills {                           //choose two
-	arcana,
-	deception,
-	insight,
-	intimidation,
-	persuasion,
-	religion
+	"arcana","deception","insight","intimidation","persuasion","religion"
 }
 
 Warlockskills {                             //choose two
-	arcana,
-	deception,
-	history,
-	intimidation,
-	investigation,
-	nature,
-	religion
+	"arcana","deception","history","intimidation","investigation","nature","religion"
 }
 
 Wizardskills{                               //choose two
-	arcana,
-	history,
-	insight,
-	investigation,
-	medicine,
-	religion
+	"arcana","history","insight","investigation","medicine","religion"
 }
