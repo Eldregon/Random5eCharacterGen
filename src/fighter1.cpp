4b96e3cd112e6373f43ecbcf4437129350125b70
Eldren <Bates.James.F@gmail.com>
2018-11-17 00:58:56 -0700

    fighter1 added too

--- src/fighter1.cpp
+++ src/fighter1.cpp
@@ -39,8 +39,8 @@ struct Martialarchetype{
 	
 	enum BMmaneuvers{
 			commanndersstrike = "(When you take an attack action on your turn, you can forgo one of your attacks and use a bonus action to direct one of your companions to strike. when you do so, choose a friendly creatures who can see or hear and expend on superiority die. that creature can immediately use its reaction to make one weapon attack, adding the superiority die to the attack's damage roll.)",
-			disarmingattack = "(when you hit a creature with a weapon attack, you can expend one superiority die to attempt to disarm the target)",
-			distractingattack = "()",
+			disarmingattack = "(when you hit a creature with a weapon attack, you can expend one superiority die to attempt to disarm the target, forcing it to drop one item of your choice that it's holding. you add the superiority die to the attack's damage roll and the target must make a STR save throw on a failed save it drops the object you choose. the object lands at its feet.)",
+			distractingattack = "(when you hit a creature with a weaopon attack, you can expend on superiority die to distract the creature, giving yout allies an opening. you add the superiority die to the attack's damage roll. the next attack roll against the tartget by an attack roll against the target by an attack other than you has adbantage if the attack is made before the start of your next turn.)",
 			evasivefootwork = "()",
 			feintingattack = "()",
 			goadingattack = "()",
